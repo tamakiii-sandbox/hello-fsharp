24acd9cf20672efcd3305cd79af4499eb5ee710d
tamakiii <tamakiii@users.noreply.github.com>
2023-03-08 21:06:42 +0900

    fix Program.fs

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System
+open Library
+
+[<EntryPoint>]
+let main args =
+    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+
+    let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
+    printfn $"Input: %0A{value}"
+    printfn $"Output: %s{json}"
+
+    0 // return an integer exit code
